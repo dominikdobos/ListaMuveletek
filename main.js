a4ca49504fce9155205b7c74922c4d58d5c7a9b0
dominikdobos <dominik.dobos33@gmail.com>
2024-05-16 11:53:44 +0200

    GET, POST, PUT, DELETE

--- main.js
+++ main.js
@@ -1,77 +1,54 @@
-import { emberekLISTA } from "./adat.js";
+// import { emberekLISTA } from "./adat.js";
+
 import { megjelenit, tablazatLetrehoz } from "./fuggvenyek.js";
 import { rendez, szures, torol } from "./adatKezelo.js";
 import { sorBeszur } from "./urlapKezelo.js";
-    /*
-    jelenisuk meg az adatainkat egy tablazatban az adatk divben
-    az urlap div-ben legyen egy urlap, amivel ilyen adatokat tudunk a tablazatba beletenni
-    a tablazat fejlecere kattintva rendezzuk a tablazatot nev szerint
-    tudjunk torolni a tablazatbol egy sort
-    legyen szuromezo, ami segitsegevel nev szerint tudjuk szurni az adatokat 
-    
-    Fuggvenyek:
+import { deleteAdat, getAdat } from "./asszinkron.js";
 
-    1. tablazatLetrehoz(lista) -> txt
-        lista alapján készít html tablazat kodot, visszaadja az elkeszult html szoveget. minden sor mellett egy torles gombbal
-    2. megjelenit(txt)
-        megjeliniti a parameterben kapott szoveget egy adott html elemben
-    3. rendez(lista) -> rendezettLista
-        megrendezi a listat adott parameter szerint, a megrendezett listat ujra megjeleniti, megrendezett listat visszaadja
-        akkor fut le amikor rakattintunk a tablazat fejlecere
-    4. szures(lista, keresoSzoveg) -> szurtLista
-        akkor fog lefutni, ha a szuro mezoben valtozas tortenik (keyup) - a listaban a nev mezoben keres egyezeseket es szur ez alapjan
-        filter
-    5. sorBeszur(lista) -> ujLista
-        ha a submit gombra kattintunk akkor fut le
-        urlap adatkat atalakitjuk objektumma es hozzaadjuk a listahoz (push)
-    6. torol(lista, index)
-        torli a lista adott sorat
-        minden sor mellett lesz egy torol gomb, amivel megkapjuk az adott sor indexet es meghivodik a torol fuggveny
-    
-    */
-    let rIrany = 1;
-    init(emberekLISTA);
-    szuresNevSzerint();
-    sorBeszur(emberekLISTA);
+let rIrany = 1;
 
-    export function init(lista){
-        var txt = tablazatLetrehoz(lista);
-        megjelenit(txt);
-        rendezesEsemeny();
-        torolEsemeny();
-    }
+getAdat("adat.json", init);
+szuresNevSzerint();
 
-    function szuresNevSzerint(){
-        const szuroElem = $("#sznev");
-        szuroElem.on("keyup", function(){
-            let szoveg = szuroElem.val();
-            init(szures(emberekLISTA, szoveg));
-        });
-    }
+export function init(lista) {
+  console.log(lista.emberekLISTA);
+  var txt = tablazatLetrehoz(lista.emberekLISTA);
+  megjelenit(txt);
+  rendezesEsemeny();
+  torolEsemeny();
+  sorBeszur(lista.emberekLISTA);
+}
 
-    /* szorgalmi: egyéb mezőkre szűrés */
+function szuresNevSzerint() {
+  const szuroElem = $("#sznev");
+  szuroElem.on("keyup", function () {
+    let szoveg = szuroElem.val();
+    init(szures(emberekLISTA, szoveg));
+  });
+}
 
+/* szorgalmi: egyéb mezőkre szűrés */
 
-    function rendezesEsemeny(){
+function rendezesEsemeny() {
+  /* ha rákattintunk a táblázat fejlécében lévő név mezőre */
+  const nevMezoElem = $(".adatok table th").eq(0);
+  nevMezoElem.on("click", function () {
+    const lista = rendez(emberekLISTA, "nev", rIrany);
+    console.log(lista);
+    init(lista);
+    rIrany *= -1;
+  });
+}
 
-        /* ha rákattintunk a táblázat fejlécében lévő név mezőre */
-        const nevMezoElem = $(".adatok table th").eq(0);
-        nevMezoElem.on("click", function(){
-            const lista = rendez(emberekLISTA, "nev", rIrany);
-            console.log(lista);
-            init(lista);
-            rIrany*=-1;
-        })
-    }
+/* szorgalmi: tudjunk rendezni a tobbi mezore is */
 
-    /* szorgalmi: tudjunk rendezni a tobbi mezore is */
-
-    function torolEsemeny(){
-        const torolELEM =$(".torol");
-        torolELEM.on("click", function(event){
-            let index = event.target.id;
-            /*              ^^az az elem,amelyik kivaltotta az esemenyt  */
-            const LISTA = torol(emberekLISTA, index);
-            init(LISTA);
-        });
-    }
+function torolEsemeny() {
+  const torolELEM = $(".torol");
+  torolELEM.on("click", function (event) {
+    let index = event.target.id;
+    // /*              ^^az az elem,amelyik kivaltotta az esemenyt  */
+    // const LISTA = torol(emberekLISTA, index);
+    // init(LISTA);
+    deleteAdat("http://localhost:3000/emberekLISTA", index);
+  });
+}
--- urlapKezelo.js
+++ urlapKezelo.js
@@ -1,22 +1,24 @@
-import { init } from "./main.js";
+import { postAdat } from "./asszinkron.js";
+// import { init } from "./main.js";
 
-    
-    
-    export function sorBeszur(lista){
-        
-        const submitELEM = $("#submit");
-        submitELEM.on("click", function(event){
-            event.preventDefault();
+export function sorBeszur(lista) {
+  const submitELEM = $("#submit");
+  submitELEM.on("click", function (event) {
+    event.preventDefault();
 
-            const adat = {
-                nev: $("#nev").val(),
-                kor: $("#kor").val(),
-                nem: $("input[name='nem']:checked").val()==="ferfi"?true:false
-            };
-            console.log($(".valid-feedback").eq(0).css("display"))
-            if($(".valid-feedback").eq(0).css("display") === "block" && $(".valid-feedback").eq(1).css("display") === "block"){
-                lista.push(adat);
-                init(lista)
-            }
-        });
+    const adat = {
+      nev: $("#nev").val(),
+      kor: $("#kor").val(),
+      nem: $("input[name='nem']:checked").val() === "ferfi" ? true : false,
+    };
+    console.log($(".valid-feedback").eq(0).css("display"));
+    if (
+      $(".valid-feedback").eq(0).css("display") === "block" &&
+      $(".valid-feedback").eq(1).css("display") === "block"
+    ) {
+      // lista.push(adat);
+      // init(lista)
+      postAdat("http://localhost:3000/emberekLISTA", adat);
     }
+  });
+}
